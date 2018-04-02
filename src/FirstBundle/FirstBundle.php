c7ce2ebba86e883f0700d7979a2ae3e69fa454bc
SkyArts <sakovskiy.sergiy@gmail.com>
2018-04-02 16:13:35 +0300

    init app

---
+++ src/FirstBundle/FirstBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace FirstBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Created by PhpStorm.
+ * User: ICGS-Support
+ * Date: 02.04.2018
+ * Time: 15:12
+ */
+
+class FirstBundle extends Bundle
+{
+
+}
